7205f2cb1b459ad100169b8ba5df41ce5430ef96
MiguelMorocho <morochomiguel56@gmail.com>
2019-06-27 22:31:37 -0500

    EjemploClases

---
+++ EjemploClases/src/ejemploclases/EjemploClases.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploclases;
+
+/**
+ *
+ * @author Miguel 
+ */
+public class EjemploClases {
+
+    String color;
+    String procesador;
+    public void establecerColor(String c){
+        color  = c;
+    }
+    public String obtenerColor(){
+        return color;
+    }
+    public void establecerProcesador(String p){
+        color  = p;
+    }
+    public String obtenerProcesador(){
+        return procesador;
+    }
+}
